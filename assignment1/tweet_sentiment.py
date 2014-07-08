6f9796eb9995f454cfcb101631e9570b4ec1fe21
jonathanau <jonathan.au@gmail.com>
2014-07-08 10:29:04 -0700

    Derive sentiment of each tweet

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,3 +1,4 @@
+import json
 import sys
 
 def hw():
@@ -6,12 +7,33 @@ def hw():
 def lines(fp):
     print str(len(fp.readlines()))
 
+def parse_afinn(input_file):
+    scores = {}
+    for line in input_file:
+        term, score = line.split("\t")
+        scores[term] = int(score)
+    return scores
+
+def score(sentiment_dictionary, text):
+    unigrams = text.split()
+    score = 0
+    for unigram in unigrams:
+        if unigram in sentiment_dictionary:
+            score += sentiment_dictionary[unigram]
+    return score
+
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+
+    sentiment_dictionary = parse_afinn(sent_file)
+    
+    for line in tweet_file:
+        tweet = json.loads(line)
+        if "text" in tweet:
+            print score(sentiment_dictionary, tweet["text"])
+        else:
+            print 0
 
 if __name__ == '__main__':
     main()
